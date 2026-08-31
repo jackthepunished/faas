84744fbb426f4c88d6aa70eef85cb5e04daf74eb
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-08-31 16:53:24 +0300

    fix(billing): make Paddle SDK import explicit

--- pkg/billing/paddle/provider.go
+++ pkg/billing/paddle/provider.go
@@ -11,7 +11,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/PaddleHQ/paddle-go-sdk/v5"
+	paddle "github.com/PaddleHQ/paddle-go-sdk/v5"
 	"github.com/onebox-faas/faas/pkg/api"
 	"github.com/onebox-faas/faas/pkg/billing"
 	"github.com/onebox-faas/faas/pkg/state"
