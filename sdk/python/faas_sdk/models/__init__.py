4e864c0b97a3a057924863c5dff814b21eee5442
Hüseyin Poyraz Küçükarslan <83272398+poyrazK@users.noreply.github.com>
2026-09-17 20:21:53 +0300

    feat: persist realtime drain operations (#2926)
    * feat: persist realtime drain operations
    
    * fix: satisfy drain spec and migration gates
    
    * fix: refresh realtime node sdk docs

--- sdk/python/faas_sdk/models/__init__.py
+++ sdk/python/faas_sdk/models/__init__.py
@@ -320,6 +320,7 @@ from .create_object_upload_route_request import CreateObjectUploadRouteRequest
 from .create_org_api_key_request import CreateOrgAPIKeyRequest
 from .create_org_api_key_request_scopes_item import CreateOrgAPIKeyRequestScopesItem
 from .create_org_request import CreateOrgRequest
+from .create_preview_request import CreatePreviewRequest
 from .create_private_network_request import CreatePrivateNetworkRequest
 from .create_project_environment_approval_request import CreateProjectEnvironmentApprovalRequest
 from .create_project_environment_request import CreateProjectEnvironmentRequest
@@ -711,6 +712,7 @@ from .managed_realtime_connection_list_response import ManagedRealtimeConnection
 from .managed_realtime_connection_response import ManagedRealtimeConnectionResponse
 from .managed_realtime_drain_request import ManagedRealtimeDrainRequest
 from .managed_realtime_drain_response import ManagedRealtimeDrainResponse
+from .managed_realtime_drain_response_status import ManagedRealtimeDrainResponseStatus
 from .managed_realtime_drain_result import ManagedRealtimeDrainResult
 from .managed_realtime_drain_result_status import ManagedRealtimeDrainResultStatus
 from .managed_realtime_endpoint_response import ManagedRealtimeEndpointResponse
@@ -1552,6 +1554,7 @@ __all__ = (
     "CreateOrgAPIKeyRequest",
     "CreateOrgAPIKeyRequestScopesItem",
     "CreateOrgRequest",
+    "CreatePreviewRequest",
     "CreatePrivateNetworkRequest",
     "CreateProjectEnvironmentApprovalRequest",
     "CreateProjectEnvironmentRequest",
@@ -1939,6 +1942,7 @@ __all__ = (
     "ManagedRealtimeConnectionResponse",
     "ManagedRealtimeDrainRequest",
     "ManagedRealtimeDrainResponse",
+    "ManagedRealtimeDrainResponseStatus",
     "ManagedRealtimeDrainResult",
     "ManagedRealtimeDrainResultStatus",
     "ManagedRealtimeEndpointResponse",
