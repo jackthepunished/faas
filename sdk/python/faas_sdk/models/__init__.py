3341a88e565bc28687803e961fc0955d8fc1691d
Hüseyin Poyraz Küçükarslan <83272398+poyrazK@users.noreply.github.com>
2026-09-19 00:07:00 +0300

    feat: expose app-owned TCP listeners via API and CLI (#3005)
    * feat: expose app-owned TCP listeners via API and CLI
    
    * docs: refresh CLI reference for TCP listeners
    
    * fix: keep VM concurrency gates alive during acquire

--- sdk/python/faas_sdk/models/__init__.py
+++ sdk/python/faas_sdk/models/__init__.py
@@ -327,6 +327,7 @@ from .create_project_environment_request import CreateProjectEnvironmentRequest
 from .create_queue_binding_request import CreateQueueBindingRequest
 from .create_queue_binding_request_mode import CreateQueueBindingRequestMode
 from .create_queue_binding_request_workload_class import CreateQueueBindingRequestWorkloadClass
+from .create_tcp_listener_request import CreateTCPListenerRequest
 from .create_tenant_surface_request import CreateTenantSurfaceRequest
 from .create_tenant_surface_request_cert_kind import CreateTenantSurfaceRequestCertKind
 from .create_trigger_batch_request import CreateTriggerBatchRequest
@@ -1108,6 +1109,8 @@ from .stream_app_logs_follow import StreamAppLogsFollow
 from .stream_app_logs_level import StreamAppLogsLevel
 from .stream_deployment_logs_follow import StreamDeploymentLogsFollow
 from .sweep_stuck_builds_response import SweepStuckBuildsResponse
+from .tcp_listener_response import TCPListenerResponse
+from .tcp_listener_response_protocol import TCPListenerResponseProtocol
 from .template_view import TemplateView
 from .template_view_category import TemplateViewCategory
 from .tenant_hostname_response import TenantHostnameResponse
@@ -1206,6 +1209,7 @@ from .update_project_request import UpdateProjectRequest
 from .update_queue_binding_request import UpdateQueueBindingRequest
 from .update_queue_binding_request_mode import UpdateQueueBindingRequestMode
 from .update_queue_binding_request_workload_class import UpdateQueueBindingRequestWorkloadClass
+from .update_tcp_listener_request import UpdateTCPListenerRequest
 from .update_trigger_request import UpdateTriggerRequest
 from .update_trigger_request_broker_poison_strategy_type_1 import UpdateTriggerRequestBrokerPoisonStrategyType1
 from .update_trigger_request_broker_poison_strategy_type_2_type_1 import (
@@ -1578,6 +1582,7 @@ __all__ = (
     "CreateQueueBindingRequest",
     "CreateQueueBindingRequestMode",
     "CreateQueueBindingRequestWorkloadClass",
+    "CreateTCPListenerRequest",
     "CreateTenantSurfaceRequest",
     "CreateTenantSurfaceRequestCertKind",
     "CreateTriggerBatchRequest",
@@ -2331,6 +2336,8 @@ __all__ = (
     "StreamAppLogsLevel",
     "StreamDeploymentLogsFollow",
     "SweepStuckBuildsResponse",
+    "TCPListenerResponse",
+    "TCPListenerResponseProtocol",
     "TemplateView",
     "TemplateViewCategory",
     "TenantHostnameResponse",
@@ -2425,6 +2432,7 @@ __all__ = (
     "UpdateQueueBindingRequest",
     "UpdateQueueBindingRequestMode",
     "UpdateQueueBindingRequestWorkloadClass",
+    "UpdateTCPListenerRequest",
     "UpdateTriggerRequest",
     "UpdateTriggerRequestBrokerPoisonStrategyType1",
     "UpdateTriggerRequestBrokerPoisonStrategyType2Type1",
