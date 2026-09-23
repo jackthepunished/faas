e211ba4975e7dc5fd596b786c24fa0b73b81c273
Hüseyin Poyraz Küçükarslan <83272398+poyrazK@users.noreply.github.com>
2026-09-23 03:43:04 +0300

    feat(services): enforce declared service bindings (#3341)
    * feat(services): enforce declared service bindings
    
    * test(api): allow typed service binding policy schema

--- sdk/python/faas_sdk/models/__init__.py
+++ sdk/python/faas_sdk/models/__init__.py
@@ -1193,6 +1193,7 @@ from .secret_scan_result import SecretScanResult
 from .security_quarantine_recovery_request import SecurityQuarantineRecoveryRequest
 from .security_quarantine_recovery_response import SecurityQuarantineRecoveryResponse
 from .security_quarantine_recovery_response_status import SecurityQuarantineRecoveryResponseStatus
+from .service_binding_policy import ServiceBindingPolicy
 from .service_replicas import ServiceReplicas
 from .service_rollout_handoff_response import ServiceRolloutHandoffResponse
 from .service_rollout_handoff_response_action import ServiceRolloutHandoffResponseAction
@@ -2549,6 +2550,7 @@ __all__ = (
     "SecurityQuarantineRecoveryRequest",
     "SecurityQuarantineRecoveryResponse",
     "SecurityQuarantineRecoveryResponseStatus",
+    "ServiceBindingPolicy",
     "ServiceReplicas",
     "ServiceRolloutHandoffResponse",
     "ServiceRolloutHandoffResponseAction",
